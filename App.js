bfc1fc20db630bdfee1835cb8577c13c6be65090
Com-Adarsh <adarshjchandran617@gmail.com>
2026-05-10 12:57:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import DetailsForm from './components/DetailsForm';
 import CreativeWall from './components/CreativeWall';
 
+
+// App.js
+const [isRegistered, setIsRegistered] = useState(() => {
+  return localStorage.getItem('isRegistered') === 'true';
+});
 function App() {
   // ആപ്പ് ലോഡ് ചെയ്യുമ്പോൾ തന്നെ ലോക്കൽ സ്റ്റോറേജ് ചെക്ക് ചെയ്യുന്നു
   const [isRegistered, setIsRegistered] = useState(() => {
